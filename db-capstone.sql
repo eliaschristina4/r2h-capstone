76de739e9df58ad1280035ec2873d4ca4020227e
Christina Elias <eliaschristina4@gmail.com>
2023-01-26 17:13:41 -0500

    thurs updates to db

--- db-capstone.sql
+++ db-capstone.sql
@@ -1,81 +1,105 @@
--- CREATE DATABASE philanthropy;
+/* CREATING DB */
+CREATE DATABASE philanthropy;
 
--- USE philanthropy;
+USE philanthropy;
 
 -- DROP DATABASE philanthropy;
 
-CREATE TABLE `philanthropy`.`profiles` (
-  `profile_id` INT NOT NULL AUTO_INCREMENT, -- pk
-  `organization_name` VARCHAR(100) NOT NULL,
-  `org_type_id` INT NOT NULL, -- fk
-  `account_type_id` INT NOT NULL, -- fk
-  `location` VARCHAR(100) NULL,
-  `contact_firstname` VARCHAR(100) NOT NULL,
-  `contact_lastname` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(150) NOT NULL,
-  `phone` VARCHAR(100) NULL,
-  `website` VARCHAR(200) NULL,
-  `interest_id` VARCHAR(100) NOT NULL,
-  
-  PRIMARY KEY (`profile_id`)
-  -- FOREIGN KEY (`account_type_id`) REFERENCES `account_types`(`type_id`),
-  -- FOREIGN KEY (`org_type_id`) REFERENCES `organization_types`(`org_type_id`)
+/* CREATING TABLES */ 
+
+CREATE TABLE `philanthropy`.`organizations` (
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `name` VARCHAR(100) NOT NULL,
+    `user_id` INT NOT NULL, -- fk
+    `type_id` INT NOT NULL, -- fk
+    `location` VARCHAR(100) NULL,
+	`contact_firstname` VARCHAR(100) NOT NULL,
+	`contact_lastname` VARCHAR(100) NOT NULL,
+	`contact_email` VARCHAR(150) NOT NULL,
+	`contact_phone` VARCHAR(100) NULL,
+	`website` VARCHAR(200) NULL,
+	`interest_id` INT NOT NULL, -- fk
+    
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user_logins`(`user_id`),
+    FOREIGN KEY (`type_id`) REFERENCES `organization_types`(`id`),
+    FOREIGN KEY (`interest_id`) REFERENCES `interests`(`id`)
 );
 
--- SELECT * FROM `profiles`;
-
--- ALTER TABLE `philanthropy`.`profiles` ADD FOREIGN KEY (`account_type_id`) REFERENCES `account_types`(`type_id`);
--- ALTER TABLE `philanthropy`.`profiles` ADD FOREIGN KEY (`org_type_id`) REFERENCES `organization_types`(`org_type_id`);
-
--- ALTER TABLE `philanthropy`.`profiles` CHANGE `interest` `interest_id` INT NOT NULL;
--- ALTER TABLE `philanthropy`.`profiles` ADD FOREIGN KEY (`interest_id`) REFERENCES `interests`(`interest_id`);
+CREATE TABLE `philanthropy`.`mentors` (
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `firstname` VARCHAR(100) NOT NULL,
+    `lastname` VARCHAR(100) NOT NULL,
+    `user_id` INT NOT NULL, -- fk
+    `location` VARCHAR(100) NULL,
+	`contact_email` VARCHAR(150) NOT NULL,
+	`contact_phone` VARCHAR(100) NULL,
+	`website` VARCHAR(200) NULL,
+	`interest_id` INT NOT NULL, -- fk
+    
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user_logins`(`user_id`),
+    FOREIGN KEY (`interest_id`) REFERENCES `interests`(`id`)
+);
 
 CREATE TABLE `philanthropy`.`organization_types` (
-	`org_type_id` INT NOT NULL AUTO_INCREMENT, -- pk
-    `org_type` VARCHAR(100) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `name` VARCHAR(100) NOT NULL,
     
-    PRIMARY KEY (`org_type_id`)
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `philanthropy`.`interests` (
-	`interest_id` INT NOT NULL AUTO_INCREMENT,
-    `interest_name` VARCHAR(100) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `name` VARCHAR(100) NOT NULL,
     
-    PRIMARY KEY (`interest_id`)
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `philanthropy`.`login_information` (
-  `profile_id` INT NOT NULL, -- fk
-  `account_type_id` INT NOT NULL, -- fk
+CREATE TABLE `philanthropy`.`user_logins` (
+  `user_id` INT NOT NULL AUTO_INCREMENT, -- pk
+  `role_id` INT NOT NULL, -- fk
   `login_email` VARCHAR(150) NOT NULL,
   `login_password` VARCHAR(150) NOT NULL,
   `date_created` DATE NOT NULL,
   
-  FOREIGN KEY (`profile_id`) REFERENCES `profiles`(`profile_id`),
-  FOREIGN KEY (`account_type_id`) REFERENCES `account_types`(`type_id`)
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`role_id`) REFERENCES `account_roles`(`id`)
 );
   
-CREATE TABLE `philanthropy`.`account_types` (
-	`type_id` INT NOT NULL AUTO_INCREMENT, -- pk
-    `type_name` VARCHAR(100) NOT NULL,
+CREATE TABLE `philanthropy`.`account_roles` (
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `role` VARCHAR(100) NOT NULL,
     
-    PRIMARY KEY (`type_id`)
+    PRIMARY KEY (`id`)
 ); 
   
 CREATE TABLE `philanthropy`.`resources` (
-  `resource_id` INT NOT NULL AUTO_INCREMENT, -- pk
-  `resource_title` VARCHAR(100) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT, -- pk
+  `title` VARCHAR(100) NOT NULL,
   `category_id` INT NOT NULL, -- fk
-  `resource_description` VARCHAR(200) NOT NULL,
+  `description` VARCHAR(200) NOT NULL,
   `monetary_value` DECIMAL(7) NULL,
   
-  PRIMARY KEY (`resource_id`),
-  FOREIGN KEY (`category_id`) REFERENCES `resource_categories`(`category_id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `resource_categories`(`id`)
 );
   
+-- need to add states table and junction table (resources AND states - meet in the middle - many-to-many -> RESEARCH)
+
 CREATE TABLE `philanthropy`.`resource_categories` (
-	`category_id` INT NOT NULL AUTO_INCREMENT, -- pk
-    `category_name` VARCHAR(100) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT, -- pk
+    `name` VARCHAR(100) NOT NULL,
     
-    PRIMARY KEY (`category_id`)
+    PRIMARY KEY (`id`)
 );
+
+/* INSERTING DATA */
+
+INSERT INTO `philanthropy`.`interests`(`name`)
+VALUES ('technology'),('education'),('retail'),('restaurant'),('logistics'),('health');
+
+INSERT INTO `philanthropy`.`account_roles`(`role`)
+VALUES ('admin'), ('hr'), ('organization'), ('mentor'); -- admin adds hr team, hr add resources and then organization/mentor user roles
+
+
