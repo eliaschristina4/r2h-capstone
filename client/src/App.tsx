a463af4d6b327d9c47df4bbaab8c86bd004dd063
Jazzuro <hjpachecocampos@gmail.com>
2023-01-28 13:35:34 -0500

    Added routes for each page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,24 @@
+// Imports
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import "./App.css";
+import { Home } from "./Pages/Home";
+import { Mentor } from "./Pages/Mentor";
+import { SignUp } from "./Pages/SignUp";
+import { Resources } from "./Pages/Resources";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signup" element={<SignUp />} />
+          <Route path="/mentor" element={<Mentor />} />
+          <Route path="/resources" element={<Resources />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
