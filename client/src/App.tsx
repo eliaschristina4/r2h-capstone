0553f559e57f5b4a4d471ddca4e68d1e8ebb8755
Jazzuro <hjpachecocampos@gmail.com>
2023-01-29 14:02:04 -0500

    Added dummy data and allowed the form to have a submit button to allow it to be passes as an object

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
-// Imports
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
+
+// Pages
 import { Home } from "./Pages/Home";
 import { Mentor } from "./Pages/Mentor";
 import { SignUp } from "./Pages/SignUp";
@@ -12,7 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="signup" element={<SignUp />} />
+          <Route path="signup/:user" element={<SignUp />} />
           <Route path="/mentor" element={<Mentor />} />
           <Route path="/resources" element={<Resources />} />
         </Routes>
