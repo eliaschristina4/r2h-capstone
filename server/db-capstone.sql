71bc6909fbe8e98e12a2119028d5f37d06db39e6
Christina Elias <eliaschristina4@gmail.com>
2023-01-30 14:27:22 -0500

    added dummy data to mentors table to test fetch in Mentors.tsx

--- server/db-capstone.sql
+++ server/db-capstone.sql
@@ -36,6 +36,20 @@ CREATE TABLE `Capstone`.`mentors` (
     FOREIGN KEY (`user_id`) REFERENCES `user_logins`(`user_id`)
 );
 
+-- DUMMY DATA
+	INSERT INTO `Capstone`.`mentors` (`fullname`, `profession`, `user_id`, `description`, `location`,`contact_email`, `contact_phone`, `website`) 
+	VALUES ('Jane Smith', 'Computer Engineer', '1', 'Smith is a world-renown Computer Engineer.', 'NC', 'janesmith@gmail.com', '555-555-555', 'janesmith.com'); 
+
+	INSERT INTO `Capstone`.`user_logins` (`role_id`,`login_email`,`login_password`,`date_created`)
+	VALUES ('4', 'janesmith@gmail.com', 'jane123', '2023-01-30');
+    
+    INSERT INTO `Capstone`.`mentors` (`fullname`, `profession`, `user_id`, `description`, `location`,`contact_email`, `contact_phone`, `website`) 
+	VALUES ('Barack Obama', 'Former President', '2', 'Obama was president of the United States. Miss him <3.', 'DC', 'bobama@whitehouse.gov', '555-555-5555', 'barackobama.com');
+    
+    INSERT INTO `Capstone`.`user_logins` (`role_id`,`login_email`,`login_password`,`date_created`)
+	VALUES ('4', 'bobama@whitehouse.gov', 'barryorules', '2023-01-30');
+-- ^^^ DUMMY DATA ^^^
+
 CREATE TABLE `Capstone`.`employees` (
 	`id` INT NOT NULL AUTO_INCREMENT, -- pk
     `fullname` VARCHAR(100) NOT NULL,
