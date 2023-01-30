7f89d3113dba03de2ac2d27c3e1396e73fdaedea
Christina Elias <eliaschristina4@gmail.com>
2023-01-30 14:25:51 -0500

    won't let me checkout main branch without committing these node mod files ig

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Home from '../src/Pages/Home';
 import Header from '../src/Components/Header';
 import Footer from '../src/Components/Footer';
-import Mentors from '../src/Pages/Mentors';
+// import Mentors from '../src/Pages/Mentors';
 
 function App() {
   return <div className="App">
@@ -12,6 +12,7 @@ function App() {
     <Header />
 
     {/* just here for coding/testing/displaying -- delete later */}
+    <Home />
     
     <Footer />
 
