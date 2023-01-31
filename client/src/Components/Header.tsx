86941c19a7398028d07d06ab216514f87d21a6ff
Christina Elias <eliaschristina4@gmail.com>
2023-01-30 20:34:19 -0500

    computer problem, committing changes to be safe

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,11 +1,23 @@
 import '../Styles/Header.css';
 
+
 export default function Header(){
+
+    function myFunction(x: any): any {
+        x.classList.toggle("change");
+        console.log(x.classList)
+      }
+
     return (
         <header className='Header'>
             <img src={require('../Images/Header/BOFA-logo-colored.png')} alt='bank of america logo' />
-
+            
             <nav>
+                <div className="container" onClick={(event) => myFunction(event)}>
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+                </div>
                 <ul>
                     <a href='#'><li>Resources</li></a>
                     <a href='#'><li>Mentor</li></a>
@@ -19,5 +31,6 @@ export default function Header(){
             </nav>
             
         </header>
+        
     );
 }
