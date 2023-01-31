6e5d66b3ee24a7d73666e620f5d71c179425df6f
Jazzuro <hjpachecocampos@gmail.com>
2023-01-31 09:21:29 -0500

    Finished form and is validated and navigation is working properly

--- client/src/Components/Sign Up/InterestsRoles.tsx
+++ client/src/Components/Sign Up/InterestsRoles.tsx
@@ -1,4 +1,3 @@
-// Sign Up Roles
 interface Roles {
   title: string;
   description: string;
@@ -10,30 +9,58 @@ interface Roles {
   role6: string;
 }
 
-export const InterestsRoles = (rolesData: Roles) => {
-  console.log(rolesData);
+interface Props {
+  rolesData: Roles;
+  handleChange: (role: string) => void;
+}
+
+export const InterestsRoles = ({ rolesData, handleChange }: Props) => {
   return (
     <>
       <p className="sign-up-title">{rolesData.title}</p>
       <p className="sign-up-description">{rolesData.description}</p>
 
       {/* Roles */}
-      <input className="form-input" type="checkbox" name="role1"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role1)}
+      ></input>
       <label htmlFor="role1">{rolesData.role1}</label>
 
-      <input className="form-input" type="checkbox" name="role2"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role2)}
+      ></input>
       <label htmlFor="role2">{rolesData.role2}</label>
 
-      <input className="form-input" type="checkbox" name="role3"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role3)}
+      ></input>
       <label htmlFor="role3">{rolesData.role3}</label>
 
-      <input className="form-input" type="checkbox" name="role4"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role4)}
+      ></input>
       <label htmlFor="role4">{rolesData.role4}</label>
 
-      <input className="form-input" type="checkbox" name="role5"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role5)}
+      ></input>
       <label htmlFor="role5">{rolesData.role5}</label>
 
-      <input className="form-input" type="checkbox" name="role6"></input>
+      <input
+        className="form-input"
+        type="checkbox"
+        onChange={() => handleChange(rolesData.role6)}
+      ></input>
       <label htmlFor="role6">{rolesData.role6}</label>
     </>
   );
