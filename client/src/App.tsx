d297bdba7df418fdac4b3cb69916fe48bb9f10e4
Christina Elias <eliaschristina4@gmail.com>
2023-02-01 11:12:17 -0500

    added routes to app, header files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+// import React from "react";
 import Header from '../src/Components/Header';
 import Footer from '../src/Components/Footer';
-import Home from '../src/Pages/Home';
-import Mentors from '../src/Pages/Mentors';
+// import Home from '../src/Pages/Home';
+// import Mentors from '../src/Pages/Mentors';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+
+// Pages
+import Home from "./Pages/Home";
+import Mentors from "./Pages/Mentors";
+// import SignUp from "./Pages/SignUp";
+// import Resources from "./Pages/Resources";
 
 function App() {
 
   return (
     <div className="App">
+    <Header />
 
-      {/* <Header /> */}
-      {/* <Mentors /> */}
-      {/* <Footer /> */}
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {/* <Route path='signup/:user' element={<SignUp />} /> */}
+          <Route path='/mentors' element={<Mentors />} />
+          {/* <Route path='/resources' element={<Resources />} /> */}
+        </Routes>
+      </Router>
 
+    <Footer />
     </div>
   );
 }
--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 
 export default function Header(){
 
+    // state variable for function below – mobile hamburger menu
     const [ menuClass, setMenuClass ] = useState('')
     const [ listClass, setListClass ] = useState('hidden')
 
+    // this function handles click event for mobile hamburger menu – drop down
     const menuClickHandler = () => {
         if (menuClass === '') {
             setMenuClass('changed');
@@ -22,23 +24,21 @@ export default function Header(){
         <header className='Header'>
 
             <img src={require('../Images/Header/BOFA-logo-colored.png')} alt='bank of america logo' id='header-logo' />
+           
             
             <nav>
+                {/* hamburger menu / nav links that only appear on smaller screens */}
                 <div id="container" className={menuClass} onClick={menuClickHandler}>
                         <div className="bar1"></div>
                         <div className="bar2"></div>
                         <div className="bar3"></div>
-                        <ul className={listClass} id='drop-down-ul' >
-                            <a href='#'><li>Resources</li></a>
-                            <a href='#'><li>Mentor</li></a>
-                            <a href='#'><li>About Us</li></a>
-                        </ul>
                 </div>
 
+                {/* large screen nav links */}
                 <ul id='static-ul'>
-                    <a href='#'><li>Resources</li></a>
-                    <a href='#'><li>Mentor</li></a>
-                    <a href='#'><li>About Us</li></a>
+                    <a href='/resources'><li>Resources</li></a>
+                    <a href='/mentors'><li>Mentors</li></a>
+                    <a href='/'><li>Home</li></a>
                 </ul>
 
                 <section className='icon-container'>
@@ -46,6 +46,11 @@ export default function Header(){
                     <img src={require('../Images/Header/profile-icon.png')} alt='profile icon'/>
                 </section>
             </nav>
+            <ul className={listClass} id='drop-down-ul' >
+                            <a href='/resources'><li>Resources</li></a>
+                            <a href='/mentors'><li>Mentor</li></a>
+                            <a href='/'><li>Home</li></a>
+                        </ul>
             
         </header>
         
