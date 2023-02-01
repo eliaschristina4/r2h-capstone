f756e67e7e004a6d55e8c4a548098eebf555f215
Christina Elias <eliaschristina4@gmail.com>
2023-02-01 11:39:10 -0500

    added route links to footer

--- client/src/Components/Footer.tsx
+++ client/src/Components/Footer.tsx
@@ -8,9 +8,9 @@ export default function Footer(){
 
             <nav>
                 <ul>
-                    <a href='#'><li>Resources</li></a>
-                    <a href='#'><li>Mentorships</li></a>
-                    <a href='#'><li>About Us</li></a>
+                    <a href='/resources'><li>Resources</li></a>
+                    <a href='/mentors'><li>Mentorships</li></a>
+                    <a href='/'><li>Home</li></a>
                 </ul>
             </nav>
 
